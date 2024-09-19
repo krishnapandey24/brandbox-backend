00bfdffc16f3ffbfcef88d56087d565e6960b824
Krishna Pandey <mastimankd@gmail.com>
2024-09-19 16:36:19 +0530

    added new APIs

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://realquick:Hello%40db123@db4free.net/realquick'
+    # SQLALCHEMY_DATABASE_URI = 'mysql://realquick:Hello%40db123@db4free.net/realquick'
+    SQLALCHEMY_DATABASE_URI = 'mysql://brandbox:Hello%40db123@brandbox.mysql.pythonanywhere-services.com/brandbox$default'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY="hello"
     MEDIA_FOLDER="D:/brandbox/brandbox/app/media"
